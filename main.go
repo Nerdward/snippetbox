9848128d1f1313fbc0e6cefed021185e9b3ab880
Nnaemeka Ohakim <ohakim.nnaemeka@gmail.com>
2023-09-14 19:54:22 +0100

    Added Error messages for non supported httprequest

--- main.go
+++ main.go
@@ -27,6 +27,17 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use the r.Method to check whether the request is using POST or not.
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
